8d34e39a78196a7dd16e49a995bfab7c229381dc
Vinceboxx <vinceboxx@gmail.com>
2023-05-28 19:02:21 +0200

    Working fronend with flask with creation, start, stop and termination of instance

---
+++ app.py
@@ -0,0 +1,263 @@
+import os
+import boto3
+from botocore.exceptions import ClientError
+from flask import Flask, render_template, request, send_file
+import paramiko
+
+app = Flask(__name__)
+
+# Configurazione del client AWS
+session = boto3.Session(profile_name='default')
+ec2_client = session.client('ec2')
+ssm_client = session.client('ssm')
+
+# Ottieni le credenziali dall'ambiente o dai profili AWS
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+aws_region = os.getenv('AWS_DEFAULT_REGION')
+
+
+# Pagina principale
+@app.route('/')
+def index():
+    instances = get_instances()
+    free_tier_remaining = get_free_tier_remaining()
+    regions = get_available_regions()
+
+    return render_template('index.html', instances=instances, free_tier_remaining=free_tier_remaining, regions=regions)
+
+
+# Ottieni tutte le istanze EC2 in esecuzione in tutte le regioni
+def get_instances():
+    instances = []
+
+    # Ottieni la lista di tutte le regioni
+    ec2_regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
+
+    for region in ec2_regions:
+        ec2 = session.client('ec2', region_name=region)
+        response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'stopped', 'stopping']}])
+
+        for reservation in response['Reservations']:
+            for instance in reservation['Instances']:
+                instance_details = {
+                    'instance_id': instance['InstanceId'],
+                    'instance_state' : instance['State']['Name'],
+                    'public_ip': instance.get('PublicIpAddress', 'N/A'),
+                    'ami_id': instance['ImageId'],
+                    'ami_name': get_ami_name(instance['ImageId'])[:21],
+                    'region': region
+                }
+                instances.append(instance_details)
+
+    return instances
+
+
+# Ottieni il nome dell'AMI dato il suo ID
+def get_ami_name(ami_id):
+    response = ec2_client.describe_images(ImageIds=[ami_id])
+    return response['Images'][0]['Name']
+
+
+# Scarica il file di connessione per l'istanza OpenVPN
+@app.route('/download/<instance_id>')
+def download_vpn_config(instance_id):
+    # Verifica che l'istanza esista e sia una istanza OpenVPN
+    vpn_instance = next((instance for instance in get_vpn_instances() if instance['instance_id'] == instance_id), None)
+    if vpn_instance:
+        # Genera il file di configurazione OpenVPN e lo salva come temp.ovpn
+        generate_vpn_config(instance_id)
+        return send_file('temp.ovpn', as_attachment=True, attachment_filename='vpn_config.ovpn')
+
+    return 'Instance not found or not an OpenVPN server'
+
+
+# Crea una nuova istanza OpenVPN Server
+@app.route('/create_instance',  methods=['POST'])
+def create_vpn_instance():
+    region = request.form.get('region')
+    # Ottieni l'AMI più recente per OpenVPN dal marketplace
+    ami_id = get_latest_ami_with_openvpn(region_name=region)
+    ec2 = session.resource('ec2', region_name=region)
+
+    key_pair_name = 'vpn-key-pair'
+    private_key_path = './ssh_keys/vpn.pem'
+
+    if not os.path.exists(private_key_path):
+
+        ec2_key_pair = ec2.create_key_pair(KeyName=key_pair_name)
+
+        # Salva la chiave privata in un file
+        with open(private_key_path, 'w') as f:
+            f.write(ec2_key_pair.key_material)
+
+        # Imposta le autorizzazioni del file della chiave privata
+        os.chmod(private_key_path, 0o400)
+
+
+    # Crea l'istanza EC2 utilizzando l'AMI
+    
+    instance = ec2.create_instances(
+        ImageId=ami_id, 
+        InstanceType='t3.micro', 
+        MinCount=1, 
+        MaxCount=1,
+        KeyName=key_pair_name
+        )[0]
+
+    # Configura automaticamente il routing del traffico dal client
+    configure_vpn_routing(instance.id)
+
+    return 'New OpenVPN instance created'
+
+
+# Ottieni l'AMI più recente per OpenVPN dal marketplace
+def get_latest_openvpn_ami(region_name):
+    ec2_client = boto3.client('ec2', region_name=region_name)
+    response = ec2_client.describe_images(
+        Owners=['aws-marketplace'],
+        Filters=[{'Name': 'product-code', 'Values': ['your-openvpn-product-code']}],
+        SortBy='CreationDate',
+        SortOrder='descending',
+        MaxResults=1
+    )
+    return response['Images'][0]['ImageId']
+
+
+# Configura automaticamente il routing del traffico dal client per l'istanza OpenVPN
+def configure_vpn_routing(instance_id):
+    # Esegui le operazioni necessarie per la configurazione automatica del routing
+    # utilizzando l'istanza OpenVPN identificata da instance_id
+    pass
+
+def get_latest_ami_with_openvpn(region_name='us-east-1'):
+    ec2_client = boto3.client('ec2', region_name=region_name)
+
+    # Ottenere la lista di AMI del Marketplace che contengono "OpenVPN" nel nome
+    response = ec2_client.describe_images(
+        Owners=['aws-marketplace'],
+        Filters=[
+            {
+                'Name': 'name',
+                'Values': ['*OpenVPN*']
+            }
+        ]
+    )
+
+    # Ordinare le AMI per data di creazione in ordine decrescente
+    sorted_images = sorted(response['Images'], key=lambda x: x['CreationDate'], reverse=True)
+
+    if sorted_images:
+        latest_ami_id = sorted_images[0]['ImageId']
+        return latest_ami_id
+
+    return None
+
+def get_available_regions():
+    ec2_client = boto3.client('ec2')
+    response = ec2_client.describe_regions()
+    regions = [region['RegionName'] for region in response['Regions']]
+    return regions
+
+# Ottieni il conteggio rimanente della free tier delle istanze EC2
+def get_free_tier_remaining():
+    billing_client = session.client('ce')
+
+    try:
+        response = billing_client.get_cost_and_usage(
+            TimePeriod={
+                'Start': '2023-01-01',
+                'End': '2023-12-31'
+            },
+            Granularity='MONTHLY',
+            Metrics=[
+                'UsageQuantity'
+            ],
+            Filter={
+                'And': [
+                    {
+                        'Dimensions': {
+                            'Key': 'USAGE_TYPE_GROUP',
+                            'Values': [
+                                'EC2: Running Hours'
+                            ]
+                        }
+                    },
+                    {
+                        'Dimensions': {
+                            'Key': 'OPERATION',
+                            'Values': [
+                                'RunInstances'
+                            ]
+                        }
+                    }
+                ]
+            }
+        )
+
+        free_tier_usage = response['ResultsByTime'][0]['Groups'][0]['Metrics']['UsageQuantity']['Amount']
+        free_tier_remaining = 750 - float(free_tier_usage)
+
+        return free_tier_remaining
+    except ClientError as e:
+        return None
+
+def execute_ssh_commands(instance_public_ip, username, private_key_path, commands):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(instance_public_ip, username=username, key_filename=private_key_path)
+        for command in commands:
+            stdin, stdout, stderr = ssh.exec_command(command)
+            output = stdout.read().decode('utf-8')
+            print(f'Output of command "{command}":')
+            print(output)
+            print('---')
+    finally:
+        ssh.close()
+
+
+@app.route('/start-instance', methods=['POST'])
+def startInstance():
+    instance_id = request.form['instance_id']
+    ec2_client = session.client('ec2')
+
+    # Avvia l'istanza specificata
+    ec2_client.start_instances(InstanceIds=[instance_id])
+
+    return 'Instance started successfully'
+
+
+@app.route('/stop-instance', methods=['POST'])
+def stopInstance():
+    instance_id = request.form['instance_id']
+    ec2_client = session.client('ec2')
+
+    # Ferma l'istanza specificata
+    ec2_client.stop_instances(InstanceIds=[instance_id])
+
+    return 'Instance stopped successfully'
+
+
+@app.route('/terminate-instance', methods=['POST'])
+def terminateInstance():
+    instance_id = request.form['instance_id']
+    ec2_client = session.client('ec2')
+
+    # Termina l'istanza specificata
+    ec2_client.terminate_instances(InstanceIds=[instance_id])
+
+    return 'Instance terminated successfully'
+
+@app.route('/instance-status/<instance_id>')
+def get_instance_status(instance_id):
+    # Logica per ottenere lo stato aggiornato dell'istanza specifica
+    instance_status = get_instance_status_from_id(instance_id)
+    
+    # Restituisci il valore di instance_status come una risposta JSON
+    return jsonify(instance_status=instance_status)
+
+
+if __name__ == '__main__':
+    app.run()
--- main.py
+++ main.py
@@ -1,1 +1,235 @@
-print ("First commit ever")
+import sys
+import os
+import tkinter as tk
+from tkinter.ttk import Combobox
+from tkinter import messagebox
+import configparser
+import boto3
+import getpass
+
+access_key_entry = None
+secret_key_entry = None
+region_entry = None
+
+def get_latest_ami_with_openvpn(region='us-east-1'):
+    ec2_client = boto3.client('ec2', region_name=region)
+
+    # Ottenere la lista di AMI del Marketplace che contengono "OpenVPN" nel nome
+    response = ec2_client.describe_images(
+        Owners=['aws-marketplace'],
+        Filters=[
+            {
+                'Name': 'name',
+                'Values': ['*OpenVPN*']
+            }
+        ]
+    )
+
+    # Ordinare le AMI per data di creazione in ordine decrescente
+    sorted_images = sorted(response['Images'], key=lambda x: x['CreationDate'], reverse=True)
+
+    if sorted_images:
+        latest_ami_id = sorted_images[0]['ImageId']
+        return latest_ami_id
+
+    return None
+
+# Eseguire la funzione per ottenere l'AMI più recente con "OpenVPN" nel nome
+latest_ami_id = get_latest_ami_with_openvpn()
+
+def create_ec2_instance(access_key, secret_key, region):
+    ec2_client = boto3.client('ec2',
+                              aws_access_key_id=access_key,
+                              aws_secret_access_key=secret_key,
+                              region_name=region)
+
+    # Creazione dell'istanza EC2
+    response = ec2_client.run_instances(
+        ImageId=get_latest_ami_with_openvpn(region=region),  # Inserire l'ID dell'AMI di OpenSSH Server
+        InstanceType='t3.micro',
+        MinCount=1,
+        MaxCount=1
+    )
+
+    instance_id = response['Instances'][0]['InstanceId']
+    return instance_id
+
+def get_aws_credentials():
+    config = configparser.ConfigParser()
+    config_file = os.path.expanduser('~/.aws/credentials')
+    config.read(config_file)
+
+    if 'default' in config:
+        return config['default'].get('aws_access_key_id'), config['default'].get('aws_secret_access_key')
+    else:
+        return None, None
+
+def get_aws_regions():
+    config = configparser.ConfigParser()
+    config_file = os.path.expanduser('~/.aws/config')
+    config.read(config_file)
+
+    regions = []
+    for section in config.sections():
+        if section.startswith('profile '):
+            region = config[section].get('region')
+            if region:
+                regions.append(region)
+
+    return regions
+
+
+def get_all_aws_regions():
+    ec2_client = boto3.client('ec2')
+    response = ec2_client.describe_regions()
+    regions = [region['RegionName'] for region in response['Regions']]
+    return regions
+
+
+
+def get_ec2_instances(access_key, secret_key, region):
+    ec2_client = boto3.client('ec2',
+                              aws_access_key_id=access_key,
+                              aws_secret_access_key=secret_key,
+                              region_name=region)
+
+    response = ec2_client.describe_instances()
+    instances = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instances.append(instance)
+
+    return instances
+
+def update_instance_status(access_key, secret_key, region):
+    instances = get_ec2_instances(access_key, secret_key, region)
+    for instance in instances:
+        instance_id = instance['InstanceId']
+        state = instance['State']['Name']
+        public_ip = instance.get('PublicIpAddress')
+        instance_text = f"Instance ID: {instance_id}\nState: {state}\nPublic IP: {public_ip}"
+        if instance_id not in instance_buttons:
+            instance_button = tk.Button(root, text=instance_text, state=tk.DISABLED)
+            instance_button.grid(sticky="W")
+            instance_buttons[instance_id] = instance_button
+        else:
+            instance_button = instance_buttons[instance_id]
+            instance_button.config(text=instance_text)
+
+def start_instance(access_key, secret_key, region, instance_id):
+    ec2_client = boto3.client('ec2',
+                              aws_access_key_id=access_key,
+                              aws_secret_access_key=secret_key,
+                              region_name=region)
+    ec2_client.start_instances(InstanceIds=[instance_id])
+    messagebox.showinfo("Success", f"Instance {instance_id} started")
+
+def stop_instance(access_key, secret_key, region, instance_id):
+    ec2_client = boto3.client('ec2',
+                              aws_access_key_id=access_key,
+                              aws_secret_access_key=secret_key,
+                              region_name=region)
+    ec2_client.stop_instances(InstanceIds=[instance_id])
+    messagebox.showinfo("Success", f"Instance {instance_id} stopped")
+
+def terminate_instance(access_key, secret_key, region, instance_id):
+    ec2_client = boto3.client('ec2',
+                              aws_access_key_id=access_key,
+                              aws_secret_access_key=secret_key,
+                              region_name=region)
+    ec2_client.terminate_instances(InstanceIds=[instance_id])
+    messagebox.showinfo("Success", f"Instance {instance_id} terminated")
+
+def run_gui():
+    access_key, secret_key = get_aws_credentials()
+    regions = get_all_aws_regions()
+
+    if access_key and secret_key:
+        root = tk.Tk()
+        root.title("AWS EC2 Manager")
+
+        access_key_label = tk.Label(root, text="Access Key:")
+        access_key_label.grid(sticky="W")
+        access_key_entry = tk.Entry(root, show="*")
+        access_key_entry.grid(sticky="W")
+
+        secret_key_label = tk.Label(root, text="Secret Key:")
+        secret_key_label.grid(sticky="W")
+        secret_key_entry = tk.Entry(root, show="*")
+        secret_key_entry.grid(sticky="W")
+
+        region_label = tk.Label(root, text="Region:")
+        region_label.grid(sticky="W")
+        region_combobox = tk.ttk.Combobox(root, values=regions, state="readonly")
+        region_combobox.grid(sticky="W")
+
+        def create_ec2_from_input():
+            access_key = access_key_entry.get()
+            secret_key = secret_key_entry.get()
+            region = region_combobox.get()
+            create_ec2_instance(access_key, secret_key, region)
+
+        create_button = tk.Button(root, text="Create EC2 Instance", command=create_ec2_from_input)
+        create_button.grid(sticky="W")
+
+        update_button = tk.Button(root, text="Update Instances", command=lambda: update_instance_status(access_key, secret_key, region_combobox.get()))
+        update_button.grid(sticky="W")
+
+        root.mainloop()
+    else:
+        print("AWS credentials not found. Please configure your credentials file.")
+
+def run_cli():
+    access_key, secret_key = get_aws_credentials()
+    regions = get_all_aws_regions()
+
+    if not access_key or not secret_key:
+        print("AWS credentials not found. Please configure your credentials file.")
+        return
+
+    print("Available regions:")
+    for i, region in enumerate(regions, start=1):
+        print(f"{i}. {region}")
+
+    region_index = int(input("Select a region (enter the corresponding number): ")) - 1
+    if region_index < 0 or region_index >= len(regions):
+        print("Invalid region selection.")
+        return
+
+    region = regions[region_index]
+
+    print(f"\nSelected region: {region}")
+
+    while True:
+        print("\n1. Create EC2 Instance")
+        print("2. Update Instances")
+        print("3. Start Instance")
+        print("4. Stop Instance")
+        print("5. Terminate Instance")
+        print("6. Exit")
+
+        choice = input("Select an option (enter the corresponding number): ")
+
+        if choice == "1":
+            create_ec2_instance(access_key, secret_key, region)
+        elif choice == "2":
+            update_instance_status(access_key, secret_key, region)
+        elif choice == "3":
+            instance_id = input("Enter the Instance ID: ")
+            start_instance(access_key, secret_key, region, instance_id)
+        elif choice == "4":
+            instance_id = input("Enter the Instance ID: ")
+            stop_instance(access_key, secret_key, region, instance_id)
+        elif choice == "5":
+            instance_id = input("Enter the Instance ID: ")
+            terminate_instance(access_key, secret_key, region, instance_id)
+        elif choice == "6":
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "--gui":
+        run_gui()
+    else:
+        run_cli()
